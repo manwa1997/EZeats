b0a46cd882ad6df3bd1dd6dc490bad61e00dd38c
Manwa Rabaya~ <manwarabaya1997@gmail.com>
2025-04-06 21:40:14 +0200

    add swaager

--- order-service/src/main.ts
+++ order-service/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { JwtAuthGuard } from './order/guards/jwt-auth.guard';
-import { Reflector } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('Order API')
+    .setDescription('The Order API description')
+    .setVersion('1.0')
+    .addTag('orders')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/orders', app, document);  
 
   await app.listen(3000);
 }
--- order-service/src/order/order.controller.ts
+++ order-service/src/order/order.controller.ts
@@ -3,13 +3,30 @@ import { Request } from 'express';
 import { JwtAuthGuard } from '../order/guards/jwt-auth.guard';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from '../order/dtos/CreateOrderDto';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('orders')  
 @Controller('orders')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post()
+  @ApiOperation({ summary: 'Create a new order' }) 
+  @ApiResponse({
+    status: 201,
+    description: 'The order has been successfully created.',
+  })  
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request. Invalid input.',
+  }) 
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Invalid or missing JWT token.',
+  })  
+  @ApiBearerAuth()  
+  @ApiBody({ type: CreateOrderDto })  
   async create(@Req() req: Request, @Body() createOrderDto: CreateOrderDto) {
     const user = req.user as { userId: number };
 
