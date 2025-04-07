38453f5395bfd08ab41590021f4883d3b5444460
Manwa Rabaya~ <manwarabaya1997@gmail.com>
2025-04-07 11:59:44 +0200

    removed unused files

--- shared/config/db.config.ts
+++
@@ -1,7 +0,0 @@
-export const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432', 10),  // Ensure it's always a number
-    database: process.env.DB_NAME || 'ezeats',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || '1234',
-  };
